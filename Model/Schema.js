c939c1ebdcbf1eb994274350a8f3d81d6eceb215
RakindGarg01 <rakind0907.be20@chitkara.edu.in>
2023-05-07 12:49:00 +0530

    .env fix

--- Model/Schema.js
+++ Model/Schema.js
@@ -1,7 +1,8 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const EmployeeSchema = mongoose.Schema({
-//     name : {type : String , required : "Employee must have a Name"},
-//     birthdate : {type : Date , required : "Employee must have Date of Birth"},
-//     gender : {type : }
-// })
+const EmployeeSchema = mongoose.Schema({
+    name : {type : String , required : "Employee must have a Name"},
+    birthdate : {type : Date , required : "Employee must have Date of Birth"},
+    gender : {type : String , }
+    
+})
